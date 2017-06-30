24ef9e3f2569cde06eea2635699218c9adcecdb0
JacobWylie <jacobwylie@gmail.com>
2017-06-30 18:32:45 +0200

    data passed through get request to campgrounds

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const express = require('express'),
 app.set('view engine', 'ejs');
 
 // Serve static js and css
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Home Route
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index');
+})
+
+// Campgrounds page route
+app.get('/campgrounds', (req, res) => {
+	// Temporary campground array before databse
+	let campgrounds = [
+		{name: "Salmon Creek", image: "https://unsplash.it/200/300/?random"},
+		{name: "Trout Land", image: "https://unsplash.it/200/300/?random"},
+		{name: "Mountain Place", image: "https://unsplash.it/200/300/?random"}
+	];
+	res.render('campgrounds', {campgrounds: campgrounds});
 })
 
 // All other routes
 app.get('*', (req,res) => {
-	res.render('error')
+	res.render('error');
 })
 
 //local server
 app.listen(3000, () => {
-	console.log('App running on localhost:3000')
+	console.log('App running on localhost:3000');
 })
